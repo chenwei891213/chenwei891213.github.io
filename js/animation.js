b20d4d64168584d61d46e0b3e432f38ad7b5a399
Chen Wei Chang <stevenfreecom1@gmail.com>
2026-03-08 22:33:56 +0800

    update web

--- js/animation.js
+++ js/animation.js
@@ -3,34 +3,19 @@
 
   doc.classList.remove('no-js')
   doc.classList.add('js')
-  
+
   // Reveal animations
   if (document.body.classList.contains('has-animations')) {
     /* global ScrollReveal */
     const sr = window.sr = ScrollReveal()
 
-    sr.reveal('.h1-large, .is-header', {
-      duration: 1000,
-      distance: '40px',
+    sr.reveal('.is-revealing', {
+      duration: 800,
+      distance: '30px',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       origin: 'bottom',
-      interval: 150
-    })
-
-    sr.reveal('.bubble-3, .bubble-4, .hero-browser-inner, .bubble-1, .bubble-2', {
-      duration: 1000,
-      scale: 0.95,
-      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-      interval: 150
-    })
-
-    sr.reveal('.feature', {
-      duration: 600,
-      distance: '40px',
-      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-      interval: 100,
-      origin: 'bottom',
-      viewFactor: 0.5
+      interval: 120,
+      viewFactor: 0.15
     })
   }
 }())
